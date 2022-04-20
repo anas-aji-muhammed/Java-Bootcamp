1c1d1fce6a4673b56c2d7c52ec8fa9bca734bdb4
anasa <anas-aji-seeroo>
2022-04-20 12:34:08 +0530

    Basics Module Added

---
+++ Basics/src/com/anasajimuhammed/developer/Main.java
@@ -0,0 +1,11 @@
+package com.anasajimuhammed.developer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Hellow Anas \n");
+        System.out.println("This is Java");
+
+
+    }
+}
